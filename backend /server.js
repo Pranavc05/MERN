2e1e58c98238836032690414826c6895ba1d3817
Pranavc05 <pranav2005.chandrasekhar@gmail.com>
2025-06-23 23:48:28 -0700

    Put and Post

--- backend /server.js
+++ backend /server.js
@@ -3,8 +3,15 @@ import express from "express"
 const app =express();
 
 app.get("/api/notes",(req,res)=>{
-    res.send("you got 5 notes")
+    res.status(200).send("you got 20 notes")
 });
+
+app.post("/api/notes",(req, res) => {
+    res.status(201).json({message:"post created successfully!"})
+})
+app.put("/api/notes/:id",(req, res) => {
+    res.status(200).json({message:"Post updated successfully!"})
+})
 
 app.listen(5001,() =>{
     console.log("Server started on PORT: 5001")
