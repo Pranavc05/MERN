3d7e69187a7ed46cbd930a258e6549ba83f5eff0
Pranavc05 <pranav2005.chandrasekhar@gmail.com>
2025-06-25 17:40:30 -0700

    Created Schema

---
+++ backend /models/Note.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+// Create a Schema
+
+//Create a model based off the schema
+
+const noteSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+         content: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true }
+
+);
