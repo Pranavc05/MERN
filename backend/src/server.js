ce743f25e232c750a69a85ff64da3fb28bdb00be
Pranavc05 <pranav2005.chandrasekhar@gmail.com>
2025-07-09 22:19:23 -0700

    Started building out the homepage UI

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
+
 
 dotenv.config();
 
@@ -11,9 +13,14 @@ const PORT = process.env.PORT || 5001;
 
 
 
-
+app.use(
+    cors({
+      origin:"http://localhost:5173",
+    })
+  )
 app.use(express.json());
 app.use(rateLimiter)
+
 
 //app.use((req,res,next)=>{
   //  console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
