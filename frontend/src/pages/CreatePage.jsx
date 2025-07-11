16ed94cfec8ef61cc66fc84a87ca4a431238be77
Pranavc05 <pranav2005.chandrasekhar@gmail.com>
2025-07-11 02:23:22 -0700

    Finished Building the HomePage UI, started on the CreateNotePage now

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,8 +1,27 @@
+import { ArrowLeftIcon } from "lucide-react";
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+  const [title,setTitle] = useState("");
+  const [content,setContent] = useState("");
+  const [loading, setLoading] = useState(false);
 
+  const handleSubmit= () => {
+
+  }
+  return <div className="min-h-screen bg-base-200">
+    <div className="container mx-auto px-4 py-8">
+      <div className="max-w-2xl mx-auto">
+        <Link to={"/"} className="btn btn-ghost mb-6">
+          <ArrowLeftIcon className="size-5" />
+          Back to Notes
+        </Link>
+        <div className="card bg-base-100">
+
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+}
 export default HomePage
